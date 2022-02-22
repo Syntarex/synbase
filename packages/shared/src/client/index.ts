9f78123de3c467cc6561b7519d105688032c2329
Syntarex <4340303+Syntarex@users.noreply.github.com>
2022-02-22 21:58:50 +0100

    Füge Client für API hinzu

---
+++ packages/shared/src/client/index.ts
@@ -0,0 +1,49 @@
+import axios, { AxiosInstance } from "axios";
+import _ from "lodash";
+import { AppClient } from "./app/app.client";
+
+export class Synbase {
+    private httpClient: AxiosInstance;
+
+    public app: AppClient;
+
+    constructor(baseUrl: string) {
+        this.httpClient = axios.create({
+            baseURL: baseUrl,
+            responseType: "json",
+        });
+
+        this.httpClient.interceptors.response.use(
+            (response) => response,
+            (error) => {
+                const message = error.response?.data?.message;
+
+                if (!_.isUndefined(message)) {
+                    if (_.isString(message)) {
+                        throw new Error(message);
+                    }
+
+                    if (_.isArray(message)) {
+                        throw new Error(message.join("\n"));
+                    }
+
+                    throw new Error(_.toString(message));
+                }
+
+                throw error;
+            },
+        );
+
+        this.app = new AppClient(this.httpClient);
+    }
+
+    public login(token: string): void {
+        this.httpClient.defaults.withCredentials = true;
+        this.httpClient.defaults.headers.common.Authorization = `Bearer ${token}`;
+    }
+
+    public logout(): void {
+        this.httpClient.defaults.withCredentials = false;
+        delete this.httpClient.defaults.headers.common.Authorization;
+    }
+}
