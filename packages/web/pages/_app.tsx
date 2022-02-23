686e2f56299e46d2282d9f2e3eca871ffe78ac0b
Syntarex <4340303+Syntarex@users.noreply.github.com>
2022-02-23 17:59:09 +0100

    Implementiere Emotion für SSR

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,9 +1,32 @@
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 import React from "react";
 import "../styles/globals.css";
+import synbaseTheme from "../styles/theme/synbase.theme";
+import createEmotionCache from "../util/create-emotion-cache.util";
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+interface IMyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
 }
+
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props: IMyAppProps) => {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={synbaseTheme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
+};
 
 export default MyApp;
