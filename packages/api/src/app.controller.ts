2099c1a823f62c37c05abc94a15d03cb58538f02
Syntarex <4340303+Syntarex@users.noreply.github.com>
2022-02-26 18:16:41 +0100

    Entferne Test-Code

--- packages/api/src/app.controller.ts
+++ packages/api/src/app.controller.ts
@@ -1,15 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
 import { ApiResource, ApiScope, ensure, IApp } from "@synbase/shared";
-import { AuthenticatedUser, Resource, Scopes } from "nest-keycloak-connect";
-import { IAuthenticatedUser } from "./auth/model/authenticated-user.model";
+import { Resource, Scopes } from "nest-keycloak-connect";
 
 @Controller("app")
 @Resource(ApiResource.App)
 export class AppController {
     @Get()
     @Scopes(ApiScope.Read)
-    public getApp(@AuthenticatedUser() user: IAuthenticatedUser): IApp {
-        console.log(user);
+    public getApp(): IApp {
         return {
             version: ensure(process.env.npm_package_version),
         };
