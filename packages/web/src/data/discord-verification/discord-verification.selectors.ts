8af8582b288d0b3f1f0f69d765560b84eb5e0fe7
Syntarex <4340303+Syntarex@users.noreply.github.com>
2022-03-02 13:34:28 +0100

    Implementiere recoil-Selektoren

--- packages/web/src/data/discord-verification/discord-verification.selectors.ts
+++ packages/web/src/data/discord-verification/discord-verification.selectors.ts
@@ -1,8 +1,43 @@
-import { IDiscordVerification, IGetDiscordVerification } from "@synbase/shared";
-import { selectorFamily } from "recoil";
+import {
+    ICreateDiscordVerification,
+    IDiscordVerification,
+    IGetDiscordVerification,
+    IUpdateDiscordVerification,
+} from "@synbase/shared";
+import { selector, selectorFamily } from "recoil";
 import { browserClient } from "../../client/browser.client";
 
 export const getAllDiscordVerifications = selectorFamily<IDiscordVerification[], IGetDiscordVerification>({
     key: "get-all-discord-verifications",
     get: (query) => async () => await browserClient.discordVerifications.getAll(query),
+});
+
+export const getMyDiscordVerification = selector<IDiscordVerification>({
+    key: "get-my-discord-verification",
+    get: async () => await browserClient.discordVerifications.getMy(),
+});
+
+export const getDiscordVerification = selectorFamily<IDiscordVerification, string>({
+    key: "get-discord-verification",
+    get: (id) => async () => await browserClient.discordVerifications.get(id),
+});
+
+export const createMyDiscordVerification = selectorFamily<IDiscordVerification, ICreateDiscordVerification>({
+    key: "create-my-discord-verification",
+    get: (body) => async () => await browserClient.discordVerifications.createMy(body),
+});
+
+export const createDiscordVerification = selectorFamily<IDiscordVerification, [string, ICreateDiscordVerification]>({
+    key: "create-discord-verification",
+    get: (data) => async () => await browserClient.discordVerifications.create(data[0], data[1]),
+});
+
+export const updateMyDiscordVerification = selectorFamily<IDiscordVerification, IUpdateDiscordVerification>({
+    key: "update-my-discord-verification",
+    get: (body) => async () => await browserClient.discordVerifications.updateMy(body),
+});
+
+export const updateDiscordVerification = selectorFamily<IDiscordVerification, [string, IUpdateDiscordVerification]>({
+    key: "update-discord-verification",
+    get: (data) => async () => await browserClient.discordVerifications.update(data[0], data[1]),
 });
