9733a69149ae46167455decd3e92f7033c05dc85
Syntarex <4340303+Syntarex@users.noreply.github.com>
2022-03-02 12:54:31 +0100

    Setze Schnittstelle auf öffentlich

--- packages/api/src/app.controller.ts
+++ packages/api/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
-import { ApiResource, ApiScope, ensure, IApp } from "@synbase/shared";
-import { Resource, Scopes } from "nest-keycloak-connect";
+import { ApiResource, ensure, IApp } from "@synbase/shared";
+import { Public, Resource } from "nest-keycloak-connect";
 
 @Controller(ApiResource.App)
 @Resource(ApiResource.App)
 export class AppController {
     @Get()
-    @Scopes(ApiScope.Read)
+    @Public()
     public read(): IApp {
         return {
             version: ensure(process.env.npm_package_version),
