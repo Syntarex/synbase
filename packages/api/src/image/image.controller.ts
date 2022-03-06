5477cad4933910a9ba4381bdae9c94509a2c78f0
Syntarex <4340303+Syntarex@users.noreply.github.com>
2022-03-06 01:54:18 +0100

    Stelle Schnittstelle zurück auf Public
    Leider ist es nicht so einfach den ImageKit-Client anzuweisen den
    Bearer-Token anzuhängen.

--- packages/api/src/image/image.controller.ts
+++ packages/api/src/image/image.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Get } from "@nestjs/common";
-import { ApiResource, ApiScope } from "@synbase/shared";
-import { Resource, Scopes } from "nest-keycloak-connect";
+import { ApiResource } from "@synbase/shared";
+import { Public } from "nest-keycloak-connect";
 import { ImageService } from "./image.service";
 
 const { Image: resource } = ApiResource;
 
+/* TODO: Wäre schön wenn man die Schnittstelle per Scope sichern könnte, jedoch erlaubt es imagekit nicht einen Custom Authorization Header mit anzuhängen */
 @Controller(resource)
-@Resource(resource)
 export class ImageController {
     constructor(private readonly imageService: ImageService) {}
 
     @Get("auth")
-    @Scopes(ApiScope.Upload)
+    @Public()
     public auth() {
         return this.imageService.getAuthentication();
     }
