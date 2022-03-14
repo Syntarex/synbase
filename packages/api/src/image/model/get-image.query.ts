484c152acb3efc093aea5769247b768c5c9a37b0
Syntarex <4340303+Syntarex@users.noreply.github.com>
2022-03-14 20:53:11 +0100

    Füge wichtigen Kommentar hinzu

--- packages/api/src/image/model/get-image.query.ts
+++ packages/api/src/image/model/get-image.query.ts
@@ -2,6 +2,7 @@ import { IGetImage } from "@synbase/shared";
 import { IsPositive, Max } from "class-validator";
 import { IsUndefinedable } from "../../util/validation/is-undefinedable.decorator";
 
+/* FIXME: Ist nicht nutzbar, schmeißt immer 400 Bad Request */
 export class GetImage implements IGetImage {
     @IsUndefinedable()
     @IsPositive()
