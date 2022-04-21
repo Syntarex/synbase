5087b9cbaa4b82a3903e9801526216126176a872
Syntarex <4340303+Syntarex@users.noreply.github.com>
2022-04-21 11:24:28 +0200

    Füge Validation für Blog-Beiträge hinzu

--- packages/shared/src/model/blog-item/index.ts
+++ packages/shared/src/model/blog-item/index.ts
@@ -1,3 +1,5 @@
+import yup from "yup";
+
 export { BlogItemFormat, IBlogItem } from "./blog-item.model";
 export { ICreateBlogItem } from "./create-blog-item.model";
 export { IGetBlogItems } from "./get-blog-items.model";
@@ -12,3 +14,19 @@ export const BlogItemConstants = {
         "^[a-z0-9]+(?:-[a-z0-9]+)*$",
     ) /* TODO: Diese Konstanten gibt es beim Profil schon. Refactore! */,
 };
+
+const { TITLE_LENGTH, SUMMARY_LENGTH, SLUG_MAX_LENGTH, SLUG_MIN_LENGTH, SLUG_REGEX } = BlogItemConstants;
+
+export const BlogItemValidation = yup.object({
+    title: yup
+        .string()
+        .max(TITLE_LENGTH, `Mach mal halblang! Maximale Zeichenanzahl: ${TITLE_LENGTH}`)
+        .required("Schenk mir bitte Beachtung. <3"),
+    summary: yup.string().max(SUMMARY_LENGTH, `Mach mal halblang! Maximale Zeichenanzahl: ${TITLE_LENGTH}`),
+    slug: yup
+        .string()
+        .max(SLUG_MAX_LENGTH, `Mach mal halblang! Maximale Zeichenanzahl: ${SLUG_MAX_LENGTH}`)
+        .min(SLUG_MIN_LENGTH, `Sprich bitte lauter. Minimale Zeichenanzahl: ${SLUG_MIN_LENGTH}`)
+        .required("Schenk mir bitte Beachtung. <3")
+        .matches(SLUG_REGEX),
+});
