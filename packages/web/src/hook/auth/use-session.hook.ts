0232bfec00ed11297e8df66f675ceaca735c26a3
Syntarex <4340303+Syntarex@users.noreply.github.com>
2022-04-27 14:10:16 +0200

    Verbessere Hook

--- packages/web/src/hook/auth/use-session.hook.ts
+++ packages/web/src/hook/auth/use-session.hook.ts
@@ -2,9 +2,14 @@ import { useSession as useNextSession } from "next-auth/react";
 import { ISession } from "../../model/auth/session.model";
 
 export const useSession = (): ISession | null | undefined => {
-    const { data } = useNextSession();
+    const { data, status } = useNextSession();
 
-    const session = data as ISession | null;
-
-    return session;
+    switch (status) {
+        case "loading":
+            return undefined;
+        case "unauthenticated":
+            return null;
+        case "authenticated":
+            return data as ISession | null;
+    }
 };
