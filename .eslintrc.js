a8d325fac7136e90214817ff909c032c3822d777
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-02-11 00:18:48 +0100

    Füge Bun als Paket-Manager hinzu

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,5 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
-    extends: [
-        "prettier",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-    ],
-    parserOptions: {
-        ecmaVersion: 2017,
-        sourceType: "module",
-        ecmaFeatures: { jsx: true },
-    },
-    rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "off",
-    },
-    settings: {
-        react: {
-            version: "detect",
-        },
-    },
+    extends: ["prettier", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
 };
