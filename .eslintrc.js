8df7413504ebaddd1b44582e0dcbee8e82978d48
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-02-12 11:57:34 +0100

    Füge Next-Linting hinzu

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
-    extends: [
-        "prettier",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
-        "plugin:@next/next/recommended",
-    ],
+    extends: ["prettier", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
 };
