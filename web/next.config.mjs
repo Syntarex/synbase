f2bcfc73af895efc48ee837fb0892b855fdbec80
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-02-19 12:22:13 +0100

    Füge Umgebungsvariablen hinzu

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        DISCORD_URL: process.env.DISCORD_URL,
+        GITHUB_URL: process.env.GITHUB_URL,
+    },
     eslint: {
         ignoreDuringBuilds: true,
     },
