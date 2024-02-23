63e26fd9900bc1dffb34057fa5d57484244e2945
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-02-23 21:06:41 +0100

    Füge Auth0-Schnittstellen hinzu

---
+++ web/src/app/api/auth/[auth0]/route.ts
@@ -0,0 +1,3 @@
+import auth0 from "@/util/auth0";
+
+export const GET = auth0.handleAuth();
---
+++ web/src/util/auth0.ts
@@ -0,0 +1,14 @@
+import { initAuth0 } from "@auth0/nextjs-auth0";
+import { getEnv } from "./env";
+
+/** Eine, für die Web-Anwendung, konfigurierte Instanz des Auth0-SDKs. */
+export default initAuth0({
+    issuerBaseURL: `https://${getEnv("AUTH0_DOMAIN")}`,
+    clientID: getEnv("AUTH0_WEB_CLIENT_ID"),
+    clientSecret: getEnv("AUTH0_WEB_CLIENT_SECRET"),
+    secret: getEnv("AUTH0_SESSION_SECRET"),
+    baseURL: getEnv("WEB_URL"),
+    authorizationParams: {
+        connection: "discord",
+    },
+});
