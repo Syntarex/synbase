8e54443e483cacc5a5cf4a4d4755c6519799d05e
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-02-24 23:16:54 +0100

    Entferne experimentelles Feature

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        typedRoutes: true,
-    },
     env: {
         DISCORD_URL: process.env.DISCORD_URL,
         GITHUB_URL: process.env.GITHUB_URL,
