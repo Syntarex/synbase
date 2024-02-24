1b092f9e0a254e53f7a3fe616a2ece1888a70bd1
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-02-24 20:53:07 +0100

    Füge typisierte Routen hinzu

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        typedRoutes: true,
+    },
     env: {
         DISCORD_URL: process.env.DISCORD_URL,
         GITHUB_URL: process.env.GITHUB_URL,
