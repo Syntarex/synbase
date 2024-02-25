036d3c78b4872ba4dc7fb60a8270844daf030be3
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-02-25 23:27:12 +0100

    Style

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,31 +5,37 @@ import "@/style/font";
 import { orbitron } from "@/style/font";
 import { getEnv } from "@/util/env";
 import { Box, Fade, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 
 const Home = () => {
     return (
-        <Stack height={"100vh"} pt={12} pb={4} px={4} alignItems={"center"} gap={8}>
+        <Stack pt={8} pb={4} px={4} alignItems={"center"} gap={8}>
             <Fade in timeout={1000}>
-                <Stack direction={"row"} flexWrap={"wrap"} alignItems={"center"} justifyContent={"center"} gap={4}>
-                    <Logo size={150} />
+                <Stack direction={"row"} flexWrap={"wrap"} alignItems={"center"} justifyContent={"center"} gap={2}>
+                    <Logo size={100} />
 
-                    <Typography variant={"h1"} fontFamily={orbitron.style.fontFamily} fontWeight={800}>
+                    <Typography variant={"h1"} fontFamily={orbitron.style.fontFamily} fontWeight={600}>
                         Synbase
                     </Typography>
                 </Stack>
             </Fade>
 
             <Fade in timeout={2000}>
-                <Stack height={"100%"} alignItems={"center"} flexGrow={1} gap={2}>
-                    <Typography>Trete dem Synbase Discord bei und werde glücklich.</Typography>
+                <Stack alignItems={"center"} gap={2}>
+                    <Stack direction={"row"} alignItems={"center"} gap={1}>
+                        <Typography>Trete dem Synbase Discord bei und werde glücklich. </Typography>
+                        <Image src={"/heart.png"} alt={"Herz"} width={20} height={20} />
+                    </Stack>
 
                     <DiscordButton href={getEnv<string>("DISCORD_URL")}>Beitreten</DiscordButton>
                 </Stack>
             </Fade>
 
-            <Box>
-                <GithubButton />
-            </Box>
+            <Fade in timeout={3000}>
+                <Box>
+                    <GithubButton />
+                </Box>
+            </Fade>
         </Stack>
     );
 };
