e571fe05f7d8b6c9cc045f20216ff3b42f26d286
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-02-28 15:40:04 +0100

    Refactore

--- web/src/component/test.tsx
+++ web/src/component/test.tsx
@@ -1,32 +1,16 @@
 "use client";
 
-import { Button } from "@mui/material";
+import { log } from "@/util/log/client";
+import { Typography } from "@mui/material";
+import { useEffect } from "react";
 
-interface TestProps {
-    slug: string;
-}
+interface TestProps {}
 
 /**
  * Einfach eine Test-Komponente, welche zum schnellen Prototyping verwendet werden kann.
  */
-export const Test = ({ slug }: TestProps) => {
-    return (
-        <Button
-            variant={"contained"}
-            onClick={() =>
-                fetch("/api/blog-post", {
-                    method: "POST",
-                    body: JSON.stringify({
-                        title: "Kurzer Beitrag zu MUI und TypeScript",
-                        description: "Einführung in die Verwendung von MUI mit TypeScript",
-                        content:
-                            "# Überschrift\n\n## H2 Überschrift\n\n### H3 Überschrift\n\n#### H4 Überschrift\n\n##### H5 Überschrift\n\n###### H6 Überschrift\n\nDas ist ein Test\n\n```code```\n\n_Fett_ __Kursiv__",
-                        slug,
-                    }),
-                })
-            }
-        >
-            Erstellen
-        </Button>
-    );
+export const Test = (props: TestProps) => {
+    useEffect(() => log("Test", props), [props]);
+
+    return <Typography>Test</Typography>;
 };
