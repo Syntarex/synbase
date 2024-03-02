3608266fc38f1fa39abf51e18cc95278a9a6c5e0
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-03-02 01:01:33 +0100

    Füge Admin Dashboard hinzu

---
+++ web/src/app/admin/page.tsx
@@ -0,0 +1,15 @@
+import "server-only";
+
+import { Stack, Typography } from "@mui/material";
+
+const AdminPage = async () => {
+    return (
+        <Stack spacing={2}>
+            <Typography variant={"h1"}>Admin Dashboard</Typography>
+
+            <Typography>TBD</Typography>
+        </Stack>
+    );
+};
+
+export default AdminPage;
