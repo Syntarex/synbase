df9aa3ec809426bffcc8707ae13042eba4101551
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-03-03 18:31:40 +0100

    Schütze Admin Dashboard auf Layout-Ebene

--- web/src/app/admin/layout.tsx
+++ web/src/app/admin/layout.tsx
@@ -1,18 +1,24 @@
 import "server-only";
 
 import { LayoutProps } from "@/model/next";
-import { QueueMusic, StackedLineChart } from "@mui/icons-material";
+import { checkScopes } from "@/util/auth0";
+import { Newspaper, QueueMusic, StackedLineChart } from "@mui/icons-material";
 import { Box, Stack, Tab, Tabs } from "@mui/material";
 
 const AdminLayout = async ({ children }: LayoutProps) => {
+    await checkScopes(["read:page:admin"], {
+        redirectTo: "/",
+    });
+
     return (
         <Stack spacing={4}>
             <Tabs variant={"scrollable"}>
-                <Tab href={"/admin"} label={"Plausible"} icon={<StackedLineChart />} />
+                <Tab href={"/admin/plausible"} label={"Plausible"} icon={<StackedLineChart />} />
                 <Tab href={"/admin/clonehero"} label={"Clone Hero"} icon={<QueueMusic />} />
+                <Tab href={"/admin/blog"} label={"Blog"} icon={<Newspaper />} />
             </Tabs>
 
-            <Box flexGrow={1}>{children}</Box>
+            <Box>{children}</Box>
         </Stack>
     );
 };
