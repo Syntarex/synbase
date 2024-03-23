64ad494688a15d262400d07302be8cf33c3f0534
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-03-24 00:54:32 +0100

    Nutze Turbopack im Dev-Modus

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
+        WEB_URL: process.env.WEB_URL,
         DISCORD_URL: process.env.DISCORD_URL,
         GITHUB_URL: process.env.GITHUB_URL,
         SYNCTUBE_URL: process.env.SYNCTUBE_URL,
@@ -8,6 +9,10 @@ const nextConfig = {
     },
     eslint: {
         ignoreDuringBuilds: true,
+    },
+    experimental: {
+        // Setze Outpout-Root in Dev-Modus auf Root-Verzeichnis des Projekts. Siehe: https://github.com/vercel/next.js/issues/56887#issuecomment-1826767117
+        outputFileTracingRoot: process.env.NODE_ENV === "development" ? "/" : undefined,
     },
 };
 
